38cff618340d0d780dfc45e26f384dc9b54bb9b4
Michel Bardelmeijer <m.bardelmeijer@gmail.com>
2020-03-04 09:12:24 +0100

    Up collision

--- resources/browsersync.blade.php
+++ resources/browsersync.blade.php
@@ -1,5 +1,3 @@
 @if (app()->environment() == 'local')
-    <script type='text/javascript' id="__bs_script__">//<![CDATA[
-    document.write("<script async src='//HOST:3000/browser-sync/browser-sync-client.js'><\/script>".replace("HOST", location.hostname));
-    //]]></script>
+    <script async src="{{ config('app.url') }}:3000/browser-sync/browser-sync-client.js"></script>
 @endif
