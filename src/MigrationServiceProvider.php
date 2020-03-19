b347f08d282faeefe4909423feddc690f06bc88e
Michel Bardelmeijer <m.bardelmeijer@gmail.com>
2020-03-19 12:53:58 +0100

    Fix migration prevent on non-local + fix migration creator with our default stubs

--- src/MigrationServiceProvider.php
+++ src/MigrationServiceProvider.php
@@ -2,20 +2,19 @@
 
 namespace Enflow\Component\Laravel;
 
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Database\Migrations\Migrator;
 use Illuminate\Support\ServiceProvider;
 
-class MigrationServiceProvider extends ServiceProvider
+class MigrationServiceProvider extends ServiceProvider implements DeferrableProvider
 {
-    protected $defer = true;
-
     public function register()
     {
-        $this->app->singleton('migration.creator', function ($app) {
+        $this->app->extend('migration.creator', function ($migrator, $app) {
             return new Migrations\MigrationCreator($app['files']);
         });
 
-        $this->app->singleton('migrator', function ($app) {
+        $this->app->extend('migrator', function ($migrator, $app) {
             return new class($app['migration.repository'], $app['db'], $app['files'], $app['events']) extends Migrator
             {
                 public function run($paths = [], array $options = [])
@@ -34,6 +33,6 @@ class MigrationServiceProvider extends ServiceProvider
 
     public function provides()
     {
-        return ['migration.creator'];
+        return ['migrator', 'migration.creator'];
     }
 }
