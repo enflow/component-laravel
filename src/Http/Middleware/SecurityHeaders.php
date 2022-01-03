b178edfc394f1f517e8d2fc4b3bd7b91bca1cf93
Michel Bardelmeijer <m.bardelmeijer@gmail.com>
2022-01-03 20:44:58 +0100

    Don't add headers double

--- src/Http/Middleware/SecurityHeaders.php
+++ src/Http/Middleware/SecurityHeaders.php
@@ -15,29 +15,29 @@ class SecurityHeaders
         $response = $next($request);
 
         // Frame options (deprecated, use content security policy instead.
-        if ($frameOptions = config('laravel.security_headers.frame_options', 'SAMEORIGIN')) {
-            $response->headers->set('X-Frame-Options', $frameOptions, false);
+        if (($frameOptions = config('laravel.security_headers.frame_options', 'SAMEORIGIN')) && !$response->headers->has('X-Frame-Options')) {
+            $response->headers->set('X-Frame-Options', $frameOptions);
         }
 
         // Referrer Policy is a new header that allows a site to control how much information the browser includes with navigations away from a document and should be set by all sites.
-        if ($request->isSecure() && ($referrerPolicy = config('laravel.security_headers.referrer_policy', 'strict-origin-when-cross-origin'))) {
-            $response->headers->set('Referrer-Policy', $referrerPolicy, false);
+        if ($request->isSecure() && ($referrerPolicy = config('laravel.security_headers.referrer_policy', 'strict-origin-when-cross-origin')) && !$response->headers->has('Referrer-Policy')) {
+            $response->headers->set('Referrer-Policy', $referrerPolicy);
         }
 
         // Expect-CT allows a site to determine if they are ready for the upcoming Chrome requirements and/or enforce their CT policy.
-        if ($expectCt = config('laravel.security_headers.expect_ct', 'max-age=0, report-uri="https://enflow.report-uri.com/r/d/ct/reportOnly"')) {
-            $response->headers->set('Expect-CT', $expectCt, false);
+        if (($expectCt = config('laravel.security_headers.expect_ct', 'max-age=0, report-uri="https://enflow.report-uri.com/r/d/ct/reportOnly"')) && !$response->headers->has('Expect-CT')) {
+            $response->headers->set('Expect-CT', $expectCt);
         }
 
         // Preventing Clickjacking
         // frame-ancestors 'self'; -> breaks Beamy as the viewer iframes the screens
-        if ($contentSecurityPolicy = config('laravel.security_headers.content_security_policy', "report-uri \"https://enflow.report-uri.com/r/d/csp/reportOnly\"")) {
+        if (($contentSecurityPolicy = config('laravel.security_headers.content_security_policy', "report-uri \"https://enflow.report-uri.com/r/d/csp/reportOnly\"")) && !$response->headers->has('Content-Security-Policy')) {
             $response->headers->set('Content-Security-Policy', $contentSecurityPolicy, false);
         }
 
         // Disable unused permissions.
-        if ($permissionPolicy = config('laravel.security_headers.permissions_policy', "accelerometer=(), gyroscope=(), magnetometer=(), microphone=(), usb=()")) {
-            $response->headers->set('Permissions-Policy', $permissionPolicy, false);
+        if (($permissionPolicy = config('laravel.security_headers.permissions_policy', "accelerometer=(), gyroscope=(), magnetometer=(), microphone=(), usb=()")) && !$response->headers->has('Permissions-Policy')) {
+            $response->headers->set('Permissions-Policy', $permissionPolicy);
         }
 
         // HSTS (activated per domain)
