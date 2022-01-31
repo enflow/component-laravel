aa53e07aa6dfb0f6b438bd7f28102a465f144d38
Michel Bardelmeijer <m.bardelmeijer@gmail.com>
2022-01-31 15:31:37 +0100

    Add hosting ID

--- config/laravel.php
+++ config/laravel.php
@@ -2,11 +2,6 @@
 
 return [
     'robots_headers' => true,
-
-    'security_headers' => [
-        'permissions_policy' => true,
-    ],
-
     'hosting_id' => env('HOSTING_ID'),
     'hsts' => env('HSTS'),
 ];
