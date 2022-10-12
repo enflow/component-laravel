c989aa86fb8b4671803d6f79bf686ebc9638d839
Michel Bardelmeijer <m.bardelmeijer@gmail.com>
2022-10-12 14:31:30 +0200

    Add redirect to well-known directory

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 if (config('laravel.security_txt', true)) {
-    Route::get('/.well-known/security.txt', fn() => response()->view('component-laravel::security-txt', [], 200, [
+    Route::redirect('security.txt', '/.well-known/security.txt');
+
+    Route::get('.well-known/security.txt', fn() => response()->view('component-laravel::security-txt', [], 200, [
         'Content-Type' => 'text/plain',
     ]));
 }
