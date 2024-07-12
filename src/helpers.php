62b3b00ce7922d5c1627780ed460cedcc8c081ec
Michel Bardelmeijer <m.bardelmeijer@gmail.com>
2024-07-12 21:30:42 +0200

    Allow closure for localize method to temporarly localize

--- src/helpers.php
+++ src/helpers.php
@@ -5,20 +5,38 @@ use Illuminate\Support\HtmlString;
 use Jenssegers\Date\Date;
 
 if (!function_exists('localize')) {
-    function localize(string $locale)
+    function localize(string $locale, ?Closure $closure = null): void
     {
-        if (strlen($locale) !== 2 && strlen($locale) !== 5) {
-            throw new InvalidArgumentException("Localize accepts a locale of exactly 2 or 5 characters, got {$locale}");
+        $set = function ($locale) {
+            if (strlen($locale) !== 2 && strlen($locale) !== 5) {
+                throw new InvalidArgumentException("Localize accepts a locale of exactly 2 or 5 characters, got {$locale}");
+            }
+
+            $parts = str_replace('_', '-', explode('-', $locale));
+            $locale = $parts[1] ?? $locale;
+
+            $locale = strtolower($locale);
+
+            app()->setLocale($locale);
+            Carbon::setLocale($locale);
+            setlocale(LC_TIME, $locale . '_' . ($locale == 'en' ? 'US' : strtoupper($locale)) . '.utf-8');
+        };
+
+        if ($closure) {
+            $currentLocale = app()->getLocale();
+
+            $set($locale);
+
+            try {
+                $closure();
+            } finally {
+                $set($currentLocale);
+            }
+
+            return;
         }
 
-        $parts = str_replace('_', '-', explode('-', $locale));
-        $locale = $parts[1] ?? $locale;
-
-        $locale = strtolower($locale);
-
-        app()->setLocale($locale);
-        Carbon::setLocale($locale);
-        setlocale(LC_TIME, $locale . '_' . ($locale == 'en' ? 'US' : strtoupper($locale)) . '.utf-8');
+        $set($locale);
     }
 }
 
